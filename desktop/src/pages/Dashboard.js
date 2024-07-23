b13361bbac8cf28ee107a1c08ebcdb0d052c1200
Шонов Азамат Маратович <strong3181@gmail.com>
2024-07-23 18:10:15 +0500

    Add order list

--- desktop/src/pages/Dashboard.js
+++ desktop/src/pages/Dashboard.js
@@ -2,12 +2,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import OrderList from '../functions/OrderList';
+import { useOrders } from '../functions/OrderContext';
 
 const Dashboard = () => {
+
+  const { orders } = useOrders();
+
   return (
     <React.Fragment>
       <div className="orders">
         <h3>Список заказов</h3>
+        {orders && <OrderList orders={orders} />}
         <OrderList />
         {/* Здесь можно отобразить список заказов */}
       </div>
