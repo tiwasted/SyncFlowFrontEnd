6b50f24b3ee45e9077c71f3f46c3e6deecd618b6
Шонов Азамат Маратович <strong3181@gmail.com>
2024-07-23 18:11:33 +0500

    Change names in order list and change naming

--- desktop/src/functions/OrderList.js
+++ desktop/src/functions/OrderList.js
@@ -1,43 +1,23 @@
-// // OrdersList.js
-// import React from 'react';
-
-// const OrdersList = () => {
-//   const orders = [];
-
-//   return (
-//     <React.Fragment>
-//       <ul>
-//         {orders.map((order, index) => (
-//           <li key={index}>{order}</li>
-//         ))}
-//       </ul>
-//     </React.Fragment>
-//   );
-// };
-
-// export default OrdersList;
-
-
-
-
 // OrderList.js
 import React from 'react';
-import { useOrders } from '../functions/OrderContext';
 
-const OrderList = () => {
-  const { orders } = useOrders();
+const OrderList = ({ orders }) => {
+  if (!orders || orders.length === 0) { // Проверяем, определен ли orders и не пуст ли он
+    return <p>Заказы не найдены</p>;
+  }
 
   return (
     <div>
       {orders.map((order) => (
-        <div key={order.id}>
+        <div key={order.id} className="order-item">
           <h4>{order.service_name}</h4>
           <p>{order.price}</p>
-          <p>{order.date}</p>
-          <p>{order.time}</p>
+          <p>{order.name}</p>
+          <p>{order.order_date}</p>
+          <p>{order.order_time}</p>
           <p>{order.address}</p>
-          <p>{order.name_of_client}</p>
-          <p>{order.phone_number}</p>
+          <p>{order.name_client}</p>
+          <p>{order.phone_number_client}</p>
           <p>{order.description}</p>
         </div>
       ))}
