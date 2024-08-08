2e23e272ca14078677ddc4b9ddd28cf1919584dd
Шонов Азамат Маратович <strong3181@gmail.com>
2024-08-08 19:55:55 +0500

    add icons for tabs

--- mobile/src/components/Task.js
+++ mobile/src/components/Task.js
@@ -267,6 +267,7 @@ const Task = ({ task, onUpdate }) => {
         <div className="task">
             <ul className='task-list'>
                 <p><strong>Тип заказа:</strong> {task.order_type}</p>
+                <p><strong>Наименование заказа:</strong>{task.order_name}</p>
                 <p><strong>Дата:</strong> {task.order_date}</p>
                 <p><strong>Время:</strong> {task.order_time}</p>
                 <p><strong>Адрес:</strong> {task.address}</p>
@@ -274,7 +275,7 @@ const Task = ({ task, onUpdate }) => {
                 <p><strong>Имя клиента:</strong> {task.name_client}</p>
                 <p><strong>Цена:</strong> {task.price}</p>
                 <p><strong>Описание:</strong> {task.description}</p>
-                <p><strong>Статус:</strong> {task.status}</p>
+                <p><strong>Статус:</strong> {task.status === 'in waiting' ? 'В ожидании' : task.status}</p>
                 <div className='task-buttons'>
                     {task.status !== 'completed' && task.status !== 'canceled' && (
                         <>
