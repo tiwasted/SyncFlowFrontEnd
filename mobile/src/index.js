251752a9d790364dbc4a9d4ca54ad6c679f67bb9
tiw <tika11124@gmail.com>
2024-09-08 14:00:57 +0500

    product deployment 08.09.24 [2]

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+    // Изменения на /mweb
     <BrowserRouter basename="/mweb">
         <App />
     </BrowserRouter>
